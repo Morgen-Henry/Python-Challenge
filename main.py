3c237af17d2055a39a5167206610950f9d806e56
Morgen-Henry <122406698+Morgen-Henry@users.noreply.github.com>
2023-04-06 23:17:31 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,45 @@
 #import csv
 import csv
-import os
 #read through csv file
-csvpath = os.path.join("PyBank","Resources", "budget_data.csv")
-with open(csvpath) as csvfile:
+with open ("PyPoll/Resources/election_data.csv") as csvfile:
     reader = csv.reader(csvfile)
     #set beginning values
-    month_count = 0
-    profit_loss_sum = 0
-    prev_prof_loss = 0
-    change = []
-    great_in = 0
-    great_de = 0
-    #for loop for each rox in csv file
+    vote_count = 0
+    list_of_candidates = {}
+    #skip headers
     next (reader)
+    #for loop for each rox in csv file
     for row in reader:
-        #add another month for each row
-        month_count = month_count + 1
-        #add the second colum of each row together to get a sum of every profit and loss
-        profit_loss = int(row[1])
-        profit_loss_sum = profit_loss_sum + profit_loss
-        #find differences between the loss/profit values
-        prof_loss_change = profit_loss - prev_prof_loss
-        prev_prof_loss = profit_loss
-        #list of changes to average later
-        change.append(prof_loss_change)
-        #check if this is the greatest increase or decrease
-        if prof_loss_change > great_in:
-            great_in = prof_loss_change
-            great_in_mon = row[0]
-        elif prof_loss_change < great_de:
-            great_de = prof_loss_change
-            great_de_mon = row[0]
+        #add to vote count
+        vote_count = vote_count + 1
+        if row[2] in list_of_candidates:
+            candidate = row[2]
+            list_of_candidates[candidate] = list_of_candidates.get(candidate, 0) + 1
         else:
-            continue
+            candidate = row[2]
+            list_of_candidates.update({candidate: 1})
 
-#remove first value of change list
-change.pop(0)
+top_vote = 0
+#print result in terminal
+print("Election Results")
+print("-------------------------")
+print("Total Votes:", vote_count)
+print("-------------------------")
+for key,value in list_of_candidates.items():
+     print(key, ":", "{:.3%}".format(value/vote_count),
+     "(", value, ")")
+     if value > top_vote:
+        top_vote = value
+        winner = key
+print("-------------------------")
+print("Winner:", winner)
+print("-------------------------")
 
-#format results
-print("Financial Analysis")
-print("----------------------------")
-print("Total Months:", month_count)
-print("Total: $", profit_loss_sum)
-print("Average Change: $", round(sum(change)/len(change),2))
-print("Greatest Increase in Profits:", great_in_mon, "($", great_in, ")")
-print("Greatest Decrease in Profits:", great_de_mon, "($", great_de, ")")
-
-#export result to text file in analysis file
-with open('PyBank/Analysis/analysis_results.csv', 'w', newline='') as file:
+#export to analysis folder
+with open('PyPoll/Analysis/analysis_results.csv', 'w', newline='') as file:
      writer = csv.writer(file)
      
-     writer.writerow(["Total Months", month_count])
-     writer.writerow(["Total", profit_loss_sum])
-     writer.writerow(["Average Change", round(sum(change)/len(change),2)])
-     writer.writerow(["Greatest Increase in Profits", great_in_mon, great_in])
-     writer.writerow(["Greatest Decrease in Profits", great_de_mon, great_de])
-
+     writer.writerow(["Total Votes", vote_count])
+     for key,value in list_of_candidates.items():
+        writer.writerow([key,"{:.3%}".format(value/vote_count), value])
+     writer.writerow(["Winner", winner])
